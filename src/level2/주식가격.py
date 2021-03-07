b93a307647e7d81b5893c98e8531338ccc1cf094
IML <shino1025@naver.com>
2021-03-07 18:04:27 +0900

    스택/큐 level2 완료

--- src/level2/주식가격.py
+++ src/level2/주식가격.py
@@ -1,17 +1,31 @@
-from collections import deque
-
 def solution(prices):
-    answer = []
-    for idx, price in enumerate(prices):
-        sec = 0
-        for i in range(idx + 1, len(prices)):
-            if price > prices[i]:
-                sec += 1
-                break
-            sec += 1
-        answer.append(sec)
+    answer, stack = [0] * len(prices), []
+    for i in range(len(prices)):
+        while stack and prices[i] < prices[stack[-1]]:
+            answer[stack[-1]] = i - stack[-1]
+            stack.pop()
+        stack.append(i)
+
+    for i in range(len(stack)):
+        answer[stack[i]] = len(prices) - stack[i] - 1
 
     return answer
 
 if __name__ == '__main__':
-    print(solution([1, 2, 3, 2, 3]))
+    print(solution([2, 1]))
+
+
+"""
+def solution(prices):
+    result=[]
+    for i, v in enumerate(prices):
+        cnt=0
+        for j in range(i+1, len(prices)):
+            if v>prices[j]:
+                cnt+=1
+                break
+            else:
+                cnt+=1
+        result.append(cnt)
+    return result
+"""
