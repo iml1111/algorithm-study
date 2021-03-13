54ef076985b87516c5661768434018b85d1697c9
IML <shin10256@gmail.com>
2021-03-13 20:02:15 +0900

    Create 디스크컨트롤러.py

---
+++ src/level3/디스크컨트롤러.py
@@ -0,0 +1,23 @@
+from heapq import heappush, heappop
+from collections import deque
+
+def solution(jobs, answer=0):
+    jobs_len = len(jobs)
+    jobs = deque(sorted(jobs))
+    cur, heap = jobs[0][0], []
+
+    while jobs or heap:
+        if not heap and cur < jobs[0][0]:
+            cur = jobs[0][0]
+        while jobs and jobs[0][0] <= cur:
+            start, time = jobs.popleft()
+            heappush(heap, (time, start))
+        if heap:
+            time, start = heappop(heap)
+            answer += (cur - start) + time
+            cur += time
+
+    return answer // jobs_len
+
+if __name__ == '__main__':
+    print(solution([[0, 5], [2, 10], [100000000000, 2]]))
