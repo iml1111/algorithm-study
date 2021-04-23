305196bee795d5f35e01a783a1b09d9500e56f11
IML <shin10256@gmail.com>
2021-04-24 02:05:52 +0900

    time: 10.642001152038574

--- src/ig/3.py
+++ src/ig/3.py
@@ -1,55 +1,57 @@
 """
-input_pad는 반드시 직(정)사각형의 형태로 온다고 전제합니다.
-벽 = #, 길 = O
+input_pad는 반드시 직사각형의 형태로 온다고 전제합니다.
 """
-
-
 def refine_pad(input_pad):
 	pad = []
-	area_id = 0
-	for i in range(len(input_pad[0])):
-		for j in range(len(input_pad)):
+	area_id, vertical_set = 0, set()
+	i_len, j_len = len(input_pad), len(input_pad[0])
+
+	for i in range(j_len):
+		for j in range(i_len):
 			if input_pad[j][i] == '#':
 				area_id += 1
 			else:
 				input_pad[j][i] = area_id
+				vertical_set.add(area_id)
 		area_id += 1
 
 	area_id = 0
-	for i in range(len(input_pad)):
-		for j in range(len(input_pad[0])):
+	for i in range(i_len):
+		for j in range(j_len):
 			if input_pad[i][j] == '#':
 				area_id += 1
 			else:
-				pad.append((area_id, input_pad[i][j]))
+				pad.append([area_id, input_pad[i][j]])
 		area_id += 1
-	return pad
 
+	pad_len, max_horizon = len(pad), pad[-1][0]
+	next_horizon = pad_len - 1
+	for i in range(pad_len - 1, -1, -1):
+		if i + 1 < pad_len - 1 and  pad[i][0] != pad[i + 1][0]:
+			next_horizon = i + 1
+		pad[i].append(next_horizon)
 
-def tracking(
-		num, start_i,
-		max_horizon, vertical_area,
-		pad, pad_len, answer
-):
-	for i in range(start_i, pad_len):
-		if pad[i][0] > max_horizon and pad[i][1] not in vertical_area:
-			tracking(
-				num + 1, i + 1,
-				pad[i][0],
-				vertical_area + [pad[i][1]],
-				pad, pad_len, answer
-			)
-	if answer['p_num'] == num:
-		answer['case'] += 1
-	elif answer['p_num'] < num:
-		answer['p_num'], answer['case'] = num, 1
+	return pad, pad_len, max_horizon, len(vertical_set)
 
 
 def solution(input_pad):
-	answer = {'p_num': 0, 'case': 0}
-	pad = refine_pad(input_pad)
-	tracking(0, 0, -1, [], pad, len(pad), answer)
-	return f"최대 {answer['p_num']}명, {answer['case']}가지"
+	p_num, case = 0, 0
+	pad, pad_len, last_horizon, max_vertical_len = refine_pad(input_pad)
+
+	def tracking(num, start_i, horizon, verticals, vertical_len):
+		nonlocal p_num, case, pad, pad_len, last_horizon, max_vertical_len
+		if horizon < last_horizon and vertical_len < max_vertical_len:
+			for i in range(start_i, pad_len):
+				h, v, next_i = pad[i]
+				if v not in verticals:
+					tracking(num+1, next_i, h, verticals+[v], vertical_len+1)
+		elif p_num < num:
+			p_num, case = num, 1
+		elif p_num == num:
+			case += 1
+
+	tracking(0, 0, -1, [], 0)
+	return f"최대 {p_num}명, {case}가지"
 
 
 if __name__ == '__main__':
@@ -64,6 +66,12 @@ if __name__ == '__main__':
 	    ["O","O","O","O",],
 	    ["O","#","O","O",],
 	    ["#","#","#","O",],
+	]))
+	print(solution([
+		["O", "O", "O", "O", ],
+		["O", "O", "O", "O", ],
+		["O", "O", "O", "O", ],
+		["O", "O", "O", "O", ],
 	]))
 	print(solution([
 		["O","O","O","#","O","O","O","O",],
@@ -83,3 +91,10 @@ if __name__ == '__main__':
 	    ["O","O", "O", "O", "#", "O", "#", "O"],
 	]))
 	print("time:", time.time() - start)
+	"""
+	최대 2명, 2가지
+	최대 4명, 2가지
+	최대 4명, 24가지
+	최대 9명, 33가지
+	최대 15명, 560가지
+	"""
