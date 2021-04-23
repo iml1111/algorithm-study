200068ffabb44bdd96915f878162612fe56a3693
IML <shin10256@gmail.com>
2021-04-24 00:22:35 +0900

    time: 15.233994245529175

--- src/ig/3.py
+++ src/ig/3.py
@@ -3,6 +3,7 @@ input_pad는 반드시 직(정)사각형의 형태로 온다고 전제합니다.
 벽 = #, 길 = O
 """
 
+
 def refine_pad(input_pad):
 	pad = []
 	area_id = 0
@@ -28,29 +29,27 @@ def refine_pad(input_pad):
 def tracking(
 		num, start_i,
 		max_horizon, vertical_area,
-		pad, pad_len
+		pad, pad_len, answer
 ):
-	p_num, case = num, 1
 	for i in range(start_i, pad_len):
 		if pad[i][0] > max_horizon and pad[i][1] not in vertical_area:
-			p_num_i, case_i = tracking(
+			tracking(
 				num + 1, i + 1,
 				pad[i][0],
 				vertical_area + [pad[i][1]],
-				pad, pad_len
+				pad, pad_len, answer
 			)
-			if p_num < p_num_i:
-				p_num, case = p_num_i, case_i
-			elif p_num == p_num_i:
-				case += case_i
-	return (p_num, case)
+	if answer['p_num'] == num:
+		answer['case'] += 1
+	elif answer['p_num'] < num:
+		answer['p_num'], answer['case'] = num, 1
 
 
 def solution(input_pad):
+	answer = {'p_num': 0, 'case': 0}
 	pad = refine_pad(input_pad)
-	return "최대 %s명, %s가지" % tracking(
-		0, 0, -1, [], pad, len(pad)
-	)
+	tracking(0, 0, -1, [], pad, len(pad), answer)
+	return f"최대 {answer['p_num']}명, {answer['case']}가지"
 
 
 if __name__ == '__main__':
