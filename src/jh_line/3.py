398b3dc55f6961df99af3c09e26fa3ea39e23be2
IML <shin10256@gmail.com>
2021-05-02 22:12:10 +0900

    Update 3.py

--- src/jh_line/3.py
+++ src/jh_line/3.py
@@ -1,7 +1,29 @@
+from heapq import heappop, heappush, heapify
+
 def solution(ads):
-	ads.sort(key= lambda x: (x[0], -x[1]))
+	wait, ads = 0, [[start, -cost] for start, cost in ads]
+	heapify(ads)
+	while ads:
+		target, candidates = heappop(ads), []
+		while ads and ads[0][0] < target[0] + 5:
+			candidates.append(heappop(ads))
+		end_time = target[0] + 5
+		for cand in candidates:
+			wait += (end_time - cand[0]) * (-cand[1])
+			heappush(ads, [end_time, cand[1]])
+	return wait
+
 
 if __name__ == '__main__':
 	print(solution([
 		[1,3], [3,2], [5,4]
+	]))
+	print(solution([
+		[0,3], [5,4]
+	]))
+	print(solution([
+		[0,1], [0,2], [6,3], [8,4]
+	]))
+	print(solution([
+		[5, 2], [2,2], [6,3], [9, 2]
 	]))
