21ad7dd714f04bd3d6dd09f2e7bc3d5325135001
IML <shin10256@gmail.com>
2021-06-20 01:00:55 +0900

    Update 셔틀버스.py

--- src/level3/셔틀버스.py
+++ src/level3/셔틀버스.py
@@ -13,21 +13,21 @@ def refine_timetable(timetable, last_bus):
 def solution(n, t, m, timetable):
     bus_table = deque([540 + i * t for i in range(n)])
     user_table = refine_timetable(timetable, bus_table[-1])
-    sheets, last_user = 0, None
+    sheets, last_user = m, None
+
     while bus_table and user_table:
-        sheets = m
         while sheets and user_table and user_table[0] <= bus_table[0]:
             last_user = user_table.popleft()
             sheets -= 1
         bus_table.popleft()
-
+        sheets = m
     if bus_table:
         return '%02d:%02d' % (bus_table[-1] // 60, bus_table[-1] % 60)
     else:
-        return '%02d:%02d' % (last_user // 60, last_user % 60 - 1)
+        return '%02d:%02d' % ((last_user - 1) // 60, (last_user - 1) % 60 )
 
 
 if __name__ == '__main__':
     print(solution(
-        2, 10, 2, ["09:10", "09:09", "08:00"]
+        1, 1, 5, ["08:00", "08:01", "08:02", "08:03"]
     ))
