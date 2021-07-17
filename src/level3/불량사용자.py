8b214990a0e884b8970859ca9354fa83ef7d45ac
IML <shin10256@gmail.com>
2021-07-17 18:48:04 +0900

    Update 불량사용자.py

--- src/level3/불량사용자.py
+++ src/level3/불량사용자.py
@@ -3,16 +3,18 @@ from itertools import product
 
 def solution(user_id, banned_id):
     patterns = [re.compile("^%s$" % i.replace("*", ".")) for i in banned_id]
-    matched = [[] for _ in range(len(patterns))]
+    patterns_len = len(patterns)
+    matched = [[] for _ in range(patterns_len)]
     for u_idx in range(len(user_id)):
-        for p_idx in range(len(patterns)):
+        for p_idx in range(patterns_len):
             if patterns[p_idx].match(user_id[u_idx]):
                 matched[p_idx].append(u_idx)
-    
-    print(matched)
-    producted = product(*matched)
-    print(*producted)
-    return
+    answer = []
+    for prod in product(*matched):
+        prod_set = set(prod)
+        if len(prod_set) == patterns_len and prod_set not in answer:
+            answer.append(prod_set)
+    return len(answer)
 
 
 if __name__ == '__main__':
