e64e7c551f46fde93a9eabc1bfb98ba606995ddf
IML <shin10256@gmail.com>
2022-08-05 18:46:05 +0900

    Create p1034.py

---
+++ src/baekjoon/gold4/p1034.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def solution(n, m, arr, num):
+    all = set([i for i in range(n)])
+    
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    n, m = list(map(int, input().split()))
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, list(input())[:-1])))
+    num = int(input())
+    solution(n, m, arr, num)
