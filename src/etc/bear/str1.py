2a1a3a72578f3958f4c3c532ba6ee1d3e844e4b9
IML <shin10256@gmail.com>
2022-12-28 00:56:07 +0900

    Update str1.py

--- src/etc/bear/str1.py
+++ src/etc/bear/str1.py
@@ -1,6 +1,6 @@
 import re
 
-def StringChallenge(strParam):
+def string_challenge(str_param):
     encoder = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     decoder = {
         **{str_i: idx for idx, str_i in enumerate(encoder)},
@@ -10,16 +10,16 @@ def StringChallenge(strParam):
         '+': lambda a, b: a + b,
         '-': lambda a, b: a - b
     }
-    strParam = re.findall("|".join(decoder.keys()), strParam)
-    decodedParam = [decoder[str_i] for str_i in strParam]
+    str_param = re.findall("|".join(decoder.keys()), str_param)
+    decoded_param = [decoder[str_i] for str_i in str_param]
 
     result, i = 0, 0
-    while isinstance(decodedParam[i], int):
-        result = 10 * result + decodedParam[i]
+    while isinstance(decoded_param[i], int):
+        result = 10 * result + decoded_param[i]
         i += 1
     op, operand = None, 0
-    for param in decodedParam[i:]:
-        if param in ['+', '-']:
+    for param in decoded_param[i:]:
+        if param in ('+', '-'):
             if op:
                 result = op(result, operand)
                 operand = 0
@@ -34,4 +34,10 @@ def StringChallenge(strParam):
         return "negative" + "".join([encoder[int(i)] for i in result[1:]])
 
 # keep this function call here
-print(StringChallenge("oneminusoneone"))
+print(string_challenge("oneminusoneone")) # 1 - 11
+print(string_challenge("threeminusfiveplusonetwo")) # 3 - 5 + 12
+print(string_challenge("threeminusfiveplusonefour")) # 3 - 5 + 14
+print(string_challenge("zerominuszero"))
+print(string_challenge("zeropluszero"))
+print(string_challenge("minuszeroplusone"))
+print(string_challenge("minuszerominusone"))
